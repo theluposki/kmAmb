6adbbc5ee41d4973d150def78f813255f2112335
theluposki <theluposki@gmail.com>
2020-12-20 19:19:21 -0300

    agora vai

---
+++ dist/precache-manifest.ed06b8bd2ad46484b6143c9eb8add3e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d76b180bac9ef619ca",
+    "url": "/css/app.436df498.css"
+  },
+  {
+    "revision": "82a550bfd0b76a059bd7fb6a0aa341c3",
+    "url": "/img/logo.82a550bf.png"
+  },
+  {
+    "revision": "061508dd186a464bb888653404224a54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99d76b180bac9ef619ca",
+    "url": "/js/app.fa8504ae.js"
+  },
+  {
+    "revision": "3bc4d82de6fb1467531d",
+    "url": "/js/chunk-vendors.9dcf2943.js"
+  },
+  {
+    "revision": "1663ec374028754dcc62542c368cdedc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
