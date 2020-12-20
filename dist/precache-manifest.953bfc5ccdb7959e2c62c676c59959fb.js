fa662e6cd75860dd7a00527e2c4b8aa2ca26f3f7
theluposki <theluposki@gmail.com>
2020-12-20 17:32:41 -0300

    fist deploy

---
+++ dist/precache-manifest.953bfc5ccdb7959e2c62c676c59959fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b02a4305dd0ab42499",
+    "url": "/css/app.436df498.css"
+  },
+  {
+    "revision": "82a550bfd0b76a059bd7fb6a0aa341c3",
+    "url": "/img/logo.82a550bf.png"
+  },
+  {
+    "revision": "2b7c0ccb6f8c1297a01cc0288d24990a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9b02a4305dd0ab42499",
+    "url": "/js/app.114c048a.js"
+  },
+  {
+    "revision": "3bc4d82de6fb1467531d",
+    "url": "/js/chunk-vendors.9dcf2943.js"
+  },
+  {
+    "revision": "1663ec374028754dcc62542c368cdedc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
