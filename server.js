9cd1b10027b048aecb8490dd992d669aa3d68005
theluposki <theluposki@gmail.com>
2020-12-20 18:30:25 -0300

    fist deploy

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express()
 
 app.use(express.static(__dirname + '/dist'))
 
+app.get('/', (req, res ) => {
+    res.sendFile(__dirname + 'dist/index.html')
+})
+
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running`)
 })
